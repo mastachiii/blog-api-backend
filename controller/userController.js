8cf9cde0c7f82fb9dce29c431e17a43c35a6a4b8
mastachii <mastachii273@gmail.com>
2025-01-20 22:13:44 +0800

    wip: User authentication

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,8 @@
 const { User } = require("../model/queries");
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+require("dotenv").config();
 
 const db = new User();
 
@@ -67,6 +69,27 @@ const createUser = [
     },
 ];
 
+async function logInUser(req, res, next) {
+    try {
+        const user = await db.getUserByUsername(req.body);
+        if (!user) return res.status(401).json({ message: "Incorrect username or password" });
+
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if (!match) return res.status(401).json({ message: "Incorrect username or password" });
+
+        const secret = process.env.SECRET;
+
+        jwt.sign({ user }, secret, { expiresIn: "1d" }, (err, token) => {
+            if (err) next(err);
+
+            res.json(token);
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
 module.exports = {
     createUser,
+    logInUser,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const controller = require("../controller/userController");
 const route = express.Router();
 
 route.post("/sign-up", controller.createUser);
+route.post("/log-in", controller.logInUser);
 
 module.exports = route;
